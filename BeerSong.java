3345589a14d086bba88a37c866a30f4ccf0ffc0a
Nicole Cox <nmcox@oakland.edu>
2017-01-29 17:31:33 -0500

    add the BeerSong.java file with empty functions

---
+++ BeerSong.java
@@ -0,0 +1,26 @@
+/*
+cit 337
+assignment 2 version control
+Nicole Cox
+ */
+package BeerSong;
+
+/**
+ *
+ * @author Nicky
+ */
+public class BeerSong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ninety_Nine_Bottles_Of_Beer();
+        
+    }
+    
+    public static void Ninety_Nine_Bottles_Of_Beer() {
+
+    
+}
